52e9710862acb6bf47b86d06b82727614bafd6c3
Tina Rezvanian <47090324+tinarezvanian@users.noreply.github.com>
2019-02-13 14:53:50 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -390,5 +390,5 @@ def password_check(password):
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port='5000')
   
--- modelcode.py
+++ modelcode.py
@@ -5,15 +5,12 @@ import random
 import efficient_apriori
 from pyvis.network import Network
 # import apyori
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from networkx.algorithms import community
 from networkx.algorithms.community import k_clique_communities
 # from io import BytesIO
-
 # import base64
 import mpld3
-
-
 # import json
 
 
@@ -27,11 +24,106 @@ def simulfunc():
     except FileNotFoundError:
         df_i = pd.read_csv('static/store_data.csv', header=None)
         df_i = df_i.astype(str)
-    # ----------------------------------------------------------------------------------------------------------
+
+    # -----------------------------------------------------------------------------------------------------------------
 
     records = []
     for i in range(0, len(df_i)):
         records.append([str(df_i.values[i, j]) for j in range(0, 20) if df_i.loc[i, j] != 'nan'])
+    # -----------------------------------------------------------------------------------------------------------------
+    #  Explore the data: use different support and confidence levels to see how number of apssociations
+    #  change and what are the associated metrics: support, confidence, conviction,  lift
+    itemsets, r = efficient_apriori.apriori(records, min_support=0.02, min_confidence=0.3)
+
+    s = []
+    cv = []
+    c = []
+    lift = []
+    nn = []
+    nd = {}
+
+    count = 0
+    for i in r:
+        s.append(i.support) # support measure
+        cv.append(i.conviction) # conviction measure
+        c.append(i.confidence) # condidence measure
+        lift.append(i.lift)
+        n = []
+        for m in i.lhs:
+            n.append(m)
+
+        for l in i.rhs:
+            n.append(l)
+        nd[count] = n
+        count += 1
+        nn.append(n)
+
+    figbar, axbar = plt.subplots(1, 1, figsize=(15, 15))
+    ax = axbar
+    x = range(len(s))
+    jn = [', '.join(a) for a in nn]
+    x_labels = jn
+    y = s
+    ax.barh(x, y, color=(0.7, 0.3, 0.3, 0.6), align='center')  # , color=color
+    plt.yticks(x, x_labels)
+    plt.tick_params(axis='both', labelsize=18)
+    plt.xlabel('Support level', fontsize=15)
+    plt.legend()
+    plt.savefig('static/barchart.png', bbox_inches='tight')
+    mpld3.save_html(figbar, 'static/barchart.html')
+    # ----------------------------------------------------------------------------------------------------------
+    # You can look at the trajectory of number of frequent combinations at different selected support level (normalized frequency):
+
+    cat_vars = [x * 0.01 for x in range(0, 20)]
+    allrules = {}
+    allsets = {}
+    lenrules = []
+    lensets = []
+    itemsets = {}
+    for var in cat_vars[1:]:
+        allsets[var] = 0
+        ti = efficient_apriori.itemsets_from_transactions(records, min_support=var)
+        itemsets[var] = ti
+        for ii in range(len(ti[0])):
+            allsets[var] = allsets[var] + len(ti[0][ii + 1])
+        lensets.append(allsets[var])
+
+    # figsupp, axsupp = plt.subplots(1, 1, figsize=(10, 8))
+    # ax = axsupp
+    # ax.plot(cat_vars[1:], lensets, '-o', color='#e6194b')
+    # plt.legend(fontsize=18, loc='upper right')
+    # plt.xlabel('Support Level', fontsize=18)
+    # plt.ylabel('Number of Frequent Baskets', fontsize=18)
+    # plt.tick_params(axis='both', labelsize=18)
+    # plt.xlim(0, 0.2)
+
+    # mpld3.save_html(figsupp, 'support.html')
+    # ----------------------------------------------------------------------------------------------------------
+    # You can look at the trajectory of number of associations found between combinations at your selected support level:
+
+    #  if you are not sure about entering min_support, min_confidence you can set up support and confidence as a
+    #  function of var (in the following for loop) such that a certain level of your desired metric
+    #  (conviction, lift, ..) is maintained
+
+    con_vars = [x * 0.10 for x in range(1, 11)]
+    con_vars = np.round(con_vars, 2)
+
+    allrules = {}
+    lenrules = []
+    for var in con_vars:
+        _, rules = efficient_apriori.apriori(records, min_support=0.01, min_confidence=var)
+        allrules[var] = rules
+        lenrules.append(len(allrules[var]))
+    # figcon, axcon = plt.subplots(1, 1)  # , figsize=(8, 6)
+    # ax = axcon
+    # ax.plot(con_vars, lenrules, '-o', label='numer of association rules ', color=(0.2, 0.7, 0.9, 0.9))
+    # plt.legend(fontsize=35, loc='upper right')
+    # plt.xlabel('Min required confidence level based on support of 1%', fontsize=35)
+    # plt.ylabel('Number of association rules', fontsize=35)
+    # plt.tick_params(axis='both', labelsize=35)
+    # mpld3.save_html(figcon, 'static/confidence.html')
+    # -----------------------------------------------------------
+    # gall: is the network of items that are associated with low confidence level, in order to see most available associations
 
     itemsets, r = efficient_apriori.apriori(records, min_support=0.01, min_confidence=0.01)
     Gall = nx.Graph()
@@ -50,8 +142,7 @@ def simulfunc():
     gall = Network()
     d = nx.degree(Gall)
     dd = dict(d)
-    # ----------------------------------------------------------------------------------------------------------
-
+    # ------------------------------------------------------------------------------------------------------------------
     c = 1
     # node_list = []
     for i in r:
@@ -66,39 +157,8 @@ def simulfunc():
                               color='#e6194b')  # (0.8,0.2,0.8,0.9) 46f0f0'#ffe119' '#f032e6'
                 gall.add_node(node_list[k], value=dd[node_list[k]], color='#e6194b')
                 gall.add_edge(node_list[j], node_list[k], color='#fabebe')
-
-
-
-    col = ['#e6194b', 'orangered', '#46f0f0', '#bcf60c', '#808080', '#911eb4',
-           '#000000', '#46f0f0', '#f032e6', '#bcf60c', '#3cb44b', '#ffe119',
-           '#4363d8', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8',
-           '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#e6194b',
-           '#bcf60c', '#808080', '#911eb4', '#46f0f0', '#f032e6',
-           '#bcf60c', '#3cb44b', '#ffe119', '#4363d8', '#fabebe', '#008080', '#e6beff',
-           '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075']
-
-    # ----------------------------------------------------------------------------------------------------------
-
-    con_vars = [x * 0.10 for x in range(1, 11)]
-    con_vars = np.round(con_vars, 2)
-
-    allrules = {}
-    lenrules = []
-    for var in con_vars:
-        _, rules = efficient_apriori.apriori(records, min_support=0.01, min_confidence=var)
-        allrules[var] = rules
-        lenrules.append(len(allrules[var]))
-    #
-    # figcon, axcon = plt.subplots(1, 1)  # , figsize=(8, 6)
-    # ax = axcon
-    # ax.plot(con_vars, lenrules, '-o', label='numer of association rules ', color=(0.2, 0.7, 0.9, 0.9))
-    # plt.legend(fontsize=35, loc='upper right')
-    # plt.xlabel('Min required confidence level based on support of 1%', fontsize=35)
-    # plt.ylabel('Number of association rules', fontsize=35)
-    # plt.tick_params(axis='both', labelsize=35)
-    # mpld3.save_html(figcon, 'static/confidence.html')
-
-    # ------------------------------------------------------------
+    # ------------------------------------------------------------------------------------------------------------------
+    # choose appropiriate min_support, min_confidence based on data:  elbow in figures support.html and confidence.html
     # min_support=0.01, min_confidence=0.3
 
     g = nx.Graph()
@@ -116,16 +176,46 @@ def simulfunc():
 
     d = nx.degree(g)
     dd = dict(d)
-    # ------------------------------------------------------------
+    # -----------------------------------------------------------------------------------------------------------------
+    #  if you want to get the red graph in slides:
+    # gn = Network()
+    # c = 1
+    # algeallrules = list(allrules.keys())[2]
+    # for i in allrules[algeallrules]:
+    #     node_list = []
+    #     for j in i.lhs:
+    #         node_list.append(j)
+    #     for j in i.rhs:
+    #         node_list.append(j)
+    #     for j in range(len(node_list) - 1):
+    #         for k in range(j + 1, len(node_list)):
+    #             #             gg.add_edge(node_list[j], node_list[k])
+    #             gn.add_node(node_list[j], value=dd[node_list[j]], color='#e6194b')
+    #             gn.add_node(node_list[k], value=dd[node_list[k]], color='#e6194b')
+    #             gn.add_edge(node_list[j], node_list[k])
+    #
+    # node_list_g = list(set(node_list))
+    # gn.save_graph('static/ggrule3percentconf.html')
+
+    # -----------------------------------------------------------------------------------------------------------------
+    # community.girvan_newman uses edgebetween ness to detect communities in graph, and finds hierachical clusters
+    # final communities are disjoint clusters, meaning they do not have any common items
     com = community.girvan_newman(g)
     communities = list(com)
     node_com = {}
     nb_com = len(communities)
-    cl = np.ceil(nb_com / 4)
+    cl = np.ceil(nb_com / 4) # there are stochastic studies that show in hierarchical clustering, 1/4th of branches in hierarchi is an acceptable cuttoff
     clusters = communities[int(cl)]
     for i in range(len(list(clusters))):
         for j in list(clusters)[i]:
             node_com[j] = i
+
+
+    col = ['#e6194b', 'orangered', '#46f0f0', '#bcf60c', '#808080','#911eb4','#000000', '#46f0f0', '#f032e6', '#bcf60c',
+           '#3cb44b', '#ffe119', '#4363d8', '#fabebe', '#008080', '#e6beff', '#9a6324','#fffac8',  '#800000', '#aaffc3',
+           '#808000', '#ffd8b1', '#000075', '#e6194b','#bcf60c', '#808080', '#911eb4', '#46f0f0', '#f032e6','#bcf60c',
+           '#3cb44b', '#ffe119', '#4363d8', '#fabebe', '#008080', '#e6beff','#9a6324', '#fffac8', '#800000', '#aaffc3',
+           '#808000', '#ffd8b1', '#000075']
 
     node_colors = []
     node_colorsg = {}
@@ -134,7 +224,7 @@ def simulfunc():
             if node_com[node] == j:
                 node_colors.append(col[j])
                 node_colorsg[node] = col[j]
-    # ------------------------
+    # -----------------------------------------------------------------------------------------------------------------
     c = 0
     clusters_out = {}
     for i in range(len(clusters)):
@@ -154,8 +244,7 @@ def simulfunc():
 
     dg = nx.degree(g)
     ddg = dict(d)
-    # ------------------------------------------------------------
-
+    # -----------------------------------------------------------------------------------------------------------------
     gg = Network()
     c = 1
     algeallrules = list(allrules.keys())[2]
@@ -174,9 +263,17 @@ def simulfunc():
 
     node_list_g = list(set(node_list))
     gg.save_graph('static/ggrule3percentconf_c.html')
+    # -----------------------------------------------------------------------------------------------------------------
+    # if you need nto export graph in json file for better visualization,  here you go:
 
-
-    # ------------------------
+    # nodes = [{'name': str(i), 'club': gg.node[i]}
+    #          for i in g.nodes()]
+    # links = [{'source': u[0], 'target': u[1]}
+    #          for u in gg.edges()]
+    # with open('graph.json', 'w') as f:
+    #     json.dump({'nodes': nodes, 'links': links},
+    #               f, indent=4,)
+    # ------------------------------------------------------------------------------------------------------------------
     cc = {}
     for i in range(3, int(len(g.nodes) / 2)):
         cc[i] = []
@@ -188,7 +285,11 @@ def simulfunc():
 
     soft_clusters = pd.DataFrame.from_dict(cc, orient='index')
     soft_clusters.reset_index(inplace=True, drop=True)
-    # ------------------------
+    # ------------------------------------------------------------------------------------------------------------------
+    #  notice in graph  that mineral water, spagetti  and .. are densly connected woth other clusters:
+    #  we may want to construct clusters that allow having shred items
+    #  k_clique_communities uses cliques of different size to detect communities in graph,
+    #  final communities are overlapping clusters, meaning they are allowed to have common items
     cc = {}
     final_package = {}
     count = 0
@@ -202,12 +303,9 @@ def simulfunc():
             cc[i].append(tt)
         cc[i] = sorted(cc[i])
 
-    final_package = cc[count]
-    # final_package_df = pd.DataFrame.from_dict(final_package)
+    # final_package = cc[count]
     soft_clusters = soft_clusters.dropna(axis=0)
-    # clusters_out_df = clusters_out_df.drop([0], axis=1)
-
-    # ------------------------
+    # -----------------------------------------------------------------------------------------------------------------
     overlapping_clusters = pd.DataFrame(columns=[0])
     for i in range(len(soft_clusters)):
         l = []
